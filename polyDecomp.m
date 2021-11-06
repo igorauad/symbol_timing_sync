63936d6535c28d2f8684a5101134865e5e180034
Igor Freire <igor.auad@gmail.com>
2021-11-06 12:06:21 -0300

    Fix unnecessary padding on polyphase decomposition
    There is no need to pad the sequence if the filter length is already a
    multiple of L. The previous implementation would add another L zero
    values unnecessarily.

--- polyDecomp.m
+++ polyDecomp.m
@@ -17,9 +17,13 @@ function [ polyFiltBank ] = polyDecomp(filt, L)
 %  Output
 % polyFiltBank -> Polyphase interpolation filter bank
 
-% First zero-pad the FIR filter to an integer multiple of L:
-nZerosToPad = L - mod(length(filt), L);
-paddedFilt = [filt zeros(1, nZerosToPad)];
+% First zero-pad the FIR filter to an integer multiple of L if necessary:
+if (mod(length(filt), L) == 0)
+    paddedFilt = filt;
+else
+    nZerosToPad = L - mod(length(filt), L);
+    paddedFilt = [filt zeros(1, nZerosToPad)];
+end
 
 % Next, split the padded sequence into "L" branches/subfilters:
 lenSubfilt = length(paddedFilt) / L;
